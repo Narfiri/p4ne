1c8b21ef20c4ebfa8cbfbcc08f3802e18e708cf3
ev.golubyatnik <retony13@gmail.com>
2022-06-21 16:18:36 +0300

    test 2

--- Lab1.1/HelloWorld.py
+++ Lab1.1/HelloWorld.py
@@ -15,4 +15,13 @@ print(8/5)
 print(8//5)
 print(8%5) #остаток после целого деления
 print(dir(s))
+l=[2,3,10,50]
+print(type(l))
+print(l[3])
+print(l.index(50))
+l.remove(50)
+print(l)
+del l[1]
+print(l)
+
 
