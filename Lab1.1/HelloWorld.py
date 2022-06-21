933da0d8eb04617f945237418d8023ee5cca1f35
ev.golubyatnik <retony13@gmail.com>
2022-06-21 16:05:05 +0300

    test

--- Lab1.1/HelloWorld.py
+++ Lab1.1/HelloWorld.py
@@ -1,3 +1,18 @@
-
+import sys
 
 print("Hello,world")
+s='my text'
+print(s)
+print(sys.getsizeof(s))
+s='another text'
+print(sys.getsizeof(s))
+print(s[2:5])
+def test(x):
+    print(x)
+    return x*x
+print (test(50)+test(20))
+print(8/5)
+print(8//5)
+print(8%5) #остаток после целого деления
+print(dir(s))
+
