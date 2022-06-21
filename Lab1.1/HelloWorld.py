af9c6555f2b98117c4617934350ce5942c1eb06c
ev.golubyatnik <retony13@gmail.com>
2022-06-21 18:07:02 +0300

    test 2

--- Lab1.1/HelloWorld.py
+++ Lab1.1/HelloWorld.py
@@ -23,5 +23,26 @@ l.remove(50)
 print(l)
 del l[1]
 print(l)
+l.insert(1,'hi')
+print(l)
+print(len(l)) #количество элементов в списке
+from matplotlib import pyplot
+from openpyxl import load_workbook
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+sheet['A'][1:]
+def getvalue(x): return x.value
+map(getvalue, sheet['A'][1:])
+pyplot.plot(list_x, list_y, label="Метка")
+
+def funk1 (x,y):
+    return x+y
+l1=[1,2,44,43]
+l2=[4,5,76,12]
+print (list(map(funk1,l1,l2)))
+print(dir(fun))
+
+
+
 
 
---
+++ Lab1.2/Lab1.2_list.py
@@ -0,0 +1,22 @@
+from openpyxl import load_workbook
+from matplotlib import pyplot
+wb = load_workbook('data_analysis_lab.xlsx')
+sheet = wb['Data']
+def getvalue(x):
+    return x.value
+list_x = list(map(getvalue, sheet['A'][1:])) #years
+print(list_x)
+
+list_y = list(map(getvalue, sheet['C'][1:])) #temp
+print(list_y)
+
+list_z = list(map(getvalue, sheet['D'][1:])) #act
+print(list_z)
+
+print(sheet)
+#print(type(sheet))
+pyplot.plot(list_x, list_y, label="Метка")
+pyplot.plot(list_x, list_z, label="Метка")
+pyplot.show()
+
+
